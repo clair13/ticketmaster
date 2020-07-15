6b463c3934ca275127e6db1d48dc841665fd827a
clair13 <theclairvoyant1313@yahoo.gr>
2020-07-15 18:44:04 +0300

    Add search ability to state

--- spec/features/searching_spec.rb
+++ spec/features/searching_spec.rb
@@ -3,12 +3,15 @@ require "rails_helper"
 RSpec.feature "Users can search for tickets matching specific creteria" do
   let(:user) { FactoryBot.create(:user) }
   let(:project) { FactoryBot.create(:project) }
+  let(:open) { State.create(name: "Open", default: true) }
+  let(:closed) { State.create(name: "Closed") }
+
   let!(:ticket_1) do
-    FactoryBot.create(:ticket, name: "Create projects", project: project, author: user, tag_names: "iteration_1")
+    FactoryBot.create(:ticket, name: "Create projects", project: project, author: user, tag_names: "iteration_1", state: open)
   end
 
   let!(:ticket_2) do
-    FactoryBot.create(:ticket, name: "Create users", project: project, author: user, tag_names: "iteration_2")
+    FactoryBot.create(:ticket, name: "Create users", project: project, author: user, tag_names: "iteration_2", state: closed)
   end
 
   before do
@@ -17,8 +20,17 @@ RSpec.feature "Users can search for tickets matching specific creteria" do
     visit project_path(project)
   end
 
-  scenario "searching by tag" do
+  xscenario "searching by tag" do
     fill_in "Search", with: "tag:iteration_1"
+    click_button "Search"
+    within("#tickets") do
+      expect(page).to have_link "Create projects"
+      expect(page).to_not have_link "Create users"
+    end
+  end
+
+  xscenario "searching by state" do
+    fill_in "Search", with: "state:Open"
     click_button "Search"
     within("#tickets") do
       expect(page).to have_link "Create projects"
