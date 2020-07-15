e1d972aa02f92f33335f31d9fe5cf47f464de23a
clair13 <theclairvoyant1313@yahoo.gr>
2020-07-15 18:49:15 +0300

    Users can click on a tag's name to see all tickets for it

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -16,7 +16,7 @@ class Ticket < ApplicationRecord
 
   searcher do
     label :tag, from: :tags, field: "name"
-    label :state, from :state, field: "name"
+    label :state, from: :state, field: "name"
   end
 
   def tag_names=(names)
